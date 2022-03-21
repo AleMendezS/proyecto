f11d97925d3ba42c0510dd6775edf36f1e361221
ALEJANDRO <ALEJANDRO@DESKTOP-DG0GMN1>
2022-03-20 20:42:48 -0600

    proyecto etapa 3

---
+++ src/test/java/com/ProyectoWeb/ProyectoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
