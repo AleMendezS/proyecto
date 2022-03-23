b1879803d15d803da242c83193470c924a1d98a2
jesusArroyoSalas <arroyosalasjesus@gmail.com>
2022-03-23 15:16:52 -0600

    modifica y elimina combos

--- src/main/java/com/ProyectoWeb/controller/ComboController.java
+++ src/main/java/com/ProyectoWeb/controller/ComboController.java
@@ -4,12 +4,14 @@
  */
 package com.ProyectoWeb.controller;
 
+import com.ProyectoWeb.domain.Combo;
 import com.ProyectoWeb.service.ComboService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -27,4 +29,27 @@ public class ComboController {
         return "/combo/listado";
     }
     
+    @GetMapping("/combo/modifica")
+    public String  nuevoCombo (Combo combo){
+        return "/combo/modifica";
+    }
+    
+    @GetMapping("/combo/modificar/{idCombo}")
+    public String modificarCombo (Combo combo, Model model){
+        var respuesta = comboService.getCombo(combo);
+        model.addAttribute("combo",respuesta);
+        return "/combo/modifica";
+    
+}
+    @PostMapping("/combo/guardar")
+    public String guardarCombo (Combo combo){
+        comboService.save(combo);
+        return "redirect:/combo/listado";
+}
+    
+    @GetMapping ("/combo/eliminar/{idCombo}")
+    public String eliminarCombo (Combo combo){
+        comboService.delete(combo);
+        return "redirect:/combo/listado";
+    }
 }
